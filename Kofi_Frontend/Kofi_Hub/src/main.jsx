c43a57e3447b6aa58ad4e3ec173cf7bf47efd422
shivamsinghnegi-ai <shivamsinghnegi2006@gmail.com>
2025-04-26 21:38:18 +0530

    render

--- Kofi_Frontend/Kofi_Hub/src/main.jsx
+++ Kofi_Frontend/Kofi_Hub/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ContactUs from './Components/ContactUs.jsx';
-import LandingPage from './Components/LandingPage.jsx';
+import LandingPage from './Components/Landingpage';
 import AboutUs from './Components/AboutUs.jsx';
 import Farms from './Components/Farms.jsx';
 import EquipmentPage from './Components/Equipment.jsx';
